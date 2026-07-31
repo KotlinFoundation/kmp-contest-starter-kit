7c916c289db7dc01099a47e99ca7349b122df961
mirzemehdi <mirzemehdi@gmail.com>
2026-07-31 11:56:14 +0200

    Bump kmpauth to 3.0.2
    Regenerate the iOS linkage package for the 3.0.2 SPM products (subpackages
    renamed _3_0_1 -> _3_0_2). Verified: resolves from Maven Central, shared JVM
    compiles, iOS framework links.

--- MobileApp/iosApp/KotlinMultiplatformLinkedPackage/Package.swift
+++ MobileApp/iosApp/KotlinMultiplatformLinkedPackage/Package.swift
@@ -13,16 +13,16 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(path: "subpackages/io_github_mirzemehdi_kmpauth_firebase_3_0_1"),
-    .package(path: "subpackages/io_github_mirzemehdi_kmpauth_google_3_0_1"),
+    .package(path: "subpackages/io_github_mirzemehdi_kmpauth_firebase_3_0_2"),
+    .package(path: "subpackages/io_github_mirzemehdi_kmpauth_google_3_0_2"),
     .package(path: "subpackages/io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
   ],
   targets: [
     .target(
       name: "KotlinMultiplatformLinkedPackage",
       dependencies: [
-        .product(name: "io_github_mirzemehdi_kmpauth_firebase_3_0_1", package: "io_github_mirzemehdi_kmpauth_firebase_3_0_1"),
-        .product(name: "io_github_mirzemehdi_kmpauth_google_3_0_1", package: "io_github_mirzemehdi_kmpauth_google_3_0_1"),
+        .product(name: "io_github_mirzemehdi_kmpauth_firebase_3_0_2", package: "io_github_mirzemehdi_kmpauth_firebase_3_0_2"),
+        .product(name: "io_github_mirzemehdi_kmpauth_google_3_0_2", package: "io_github_mirzemehdi_kmpauth_google_3_0_2"),
         .product(name: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0", package: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
       ]
     )
